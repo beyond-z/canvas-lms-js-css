01dd005da90c6a965386f33941d7e95caa8c4ee4
Brian Sadler <brian.sadler@beyondz.org>
2015-03-17 16:15:35 -0700

    Add skeleton Global Javascript and CSS files.

---
+++ bz_custom.js
@@ -0,0 +1,6 @@
+/**
+ *  Add your custom beyondz Javascript for the Canvas LMS in this file.
+ *  This is configured on the Canvas admin as a Global Javascript include.
+ *  See this for more info: https://guides.instructure.com/m/4214/l/41896-how-do-i-add-custom-javascript-and-css-files-to-my-account
+ *
+ */
