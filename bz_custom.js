6213fa922148b185102b7273311756bf14a5584e
ehudtal <ehud.tal@hefetzandtal.com>
2015-03-19 11:50:30 -0400

    Update bz_custom.js
    Testing basic JS and JQuery

--- bz_custom.js
+++ bz_custom.js
@@ -4,3 +4,15 @@
  *  See this for more info: https://guides.instructure.com/m/4214/l/41896-how-do-i-add-custom-javascript-and-css-files-to-my-account
  *
  */
+ 
+ //testing js:
+ var logo = document.getElementByID("intro-logo");
+ logo.onClick=function(e){
+   alert("hello");
+ }
+ 
+ //testing jquery
+$.noConflict();
+jQuery( document ).ready(function( $ ) {
+  alert("jQ in da haus!")
+}); jQuery
