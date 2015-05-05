555a1bc0c8a5438ff824e6028820c946d74f4e04
ehudtal <ehud78@gmail.com>
2015-05-05 13:24:33 -0400

    commented out all js

--- bz_custom.js
+++ bz_custom.js
@@ -3,7 +3,7 @@
  *  This is configured on the Canvas admin as a Global Javascript include.
  *  See this for more info: https://guides.instructure.com/m/4214/l/41896-how-do-i-add-custom-javascript-and-css-files-to-my-account
  *
- */
+ *
  
  //testing js:
  console.log("js working");
@@ -12,15 +12,18 @@
 $.noConflict();
 jQuery( document ).ready(function( $ ) {
 	console.log("jQ working");
-	/*
+	
 	$('.context-course_11 #question_389_question_text ol li').prepend('<span class="dynamic"></span>');
-	$('.context-course_11 #question_388_question_text input').each(function(i){
+	$('.context-course_11 #question_388_question_text input, .context-course_11 #question_434_question_text input').each(function(i){
 		$(this).change(function(){
 			var t = $(this).val()+': '; console.log(t);
 			$('#question_389_question_text ol li').eq(i).children('.dynamic').text(t);
 			//
 		});
 	});
-	*/
+	
 
 }); jQuery
+
+
+*/
