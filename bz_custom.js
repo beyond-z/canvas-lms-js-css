061aa2246949cca2e9759f4eac8fbfafdbc7bbfa
Brian Sadler <sadler.brian@gmail.com>
2015-05-20 20:22:13 -0700

    Update JS for new SFER 11 week course.

--- bz_custom.js
+++ bz_custom.js
@@ -18,12 +18,12 @@ jQuery( document ).ready(function() {
 	console.log("jQ working");
 	
 	/* Improve Priorities Quiz */  
-	jQuery('.context-course_11 #question_482_question_text ol li, .context-course_12 #question_555_question_text ol li').prepend('<span class="dynamic"></span>');
-	jQuery('.context-course_11 #question_481_question_text input, .context-course_12 #question_554_question_text input').each(function(i){
+	jQuery('.context-course_11 #question_482_question_text ol li, .context-course_12 #question_619_question_text ol li').prepend('<span class="dynamic"></span>');
+	jQuery('.context-course_11 #question_481_question_text input, .context-course_15 #question_618_question_text input').each(function(i){
 		jQuery(this).change(function(){
 			console.log('changing big rock');
 			var t = jQuery(this).val()+': '; console.log(t);
-			jQuery('.context-course_11 #question_482_question_text ol li, .context-course_11 #question_555_question_text ol li').eq(i).children('.dynamic').text(t);
+			jQuery('.context-course_11 #question_482_question_text ol li, .context-course_15 #question_619_question_text ol li').eq(i).children('.dynamic').text(t);
 		});
 	});
 	/**/
