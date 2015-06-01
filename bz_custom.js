bf48999ad7449555534b0d5f9f4fc3650fa025c6
ehudtal <ehud78@gmail.com>
2015-06-01 17:44:46 -0400

    disabled js tooltip (commented out)

--- bz_custom.js
+++ bz_custom.js
@@ -15,8 +15,29 @@ css-files-to-my-account
 jQuery.noConflict();
 */
 jQuery( document ).ready(function() {
-	console.log("jQ working");
-	
+	console.log("jQ working v9");
+	/* Tooltips 
+	jQuery('.bz-has-tooltip').hover(function(e){
+		console.log(e);
+        // Hover over item to show tooltip:
+        var title = jQuery(this).attr('title');
+		if (title) {
+			console.log(title);
+			jQuery(this).data('tipText', title);//.removeAttr('alt');
+			jQuery('<p class="bz-tooltip"></p>')
+			.text(title)
+			.appendTo(jQuery(this).parents('td'))
+			//.appendTo(jQuery('#show-tips'))
+			.fadeIn('slow');
+		}
+	}, function(e) {
+		console.log(e);
+        // remove the tooltip on mouseout:
+		if(jQuery(this).data('tipText')) {
+	        //jQuery(this).attr('alt', jQuery(this).data('tipText'));
+    	    jQuery('.bz-tooltip').remove();
+		}
+	});
 	/* Improve Priorities Quiz */  
 	jQuery('.context-course_11 #question_482_question_text ol li, .context-course_15 #question_619_question_text ol li').prepend('<span class="dynamic"></span>');
 	jQuery('.context-course_11 #question_481_question_text input, .context-course_15 #question_618_question_text input').each(function(i){
@@ -27,24 +48,8 @@ jQuery( document ).ready(function() {
 		});
 	});
 	/**/
-	/* Tooltips */
-	jQuery('.bz-has-tooltip').hover(function(){
-        // Hover over item to show tooltip:
-        var title = jQuery(this).attr('alt') || jQuery(this).attr('title');
-		if (title) {
-			jQuery(this).data('tipText', title);//.removeAttr('alt');
-			jQuery('<p class="bz-tooltip"></p>')
-			.text(title)
-			.appendTo(jQuery(this).parent())
-			.fadeIn('slow');
-		}
-	}, function() {
-        // remove the tooltip on mouseout:
-		if(jQuery(this).data('tipText')) {
-	        //jQuery(this).attr('alt', jQuery(this).data('tipText'));
-    	    jQuery('.bz-tooltip').remove();
-		}
-	});
+	
+	
 
 	/* Interactive diagram about design thinking 
 	jQuery('div#design-thinking-chart').replaceWith('<canvas id="design-thinking-chart" />');
